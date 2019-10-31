da4bf50d4481a21dbad7b495da75eee808c0a98a
Spearstorms <47761419+Spearstorms@users.noreply.github.com>
2019-10-31 14:09:27 -0400

    Throws/catches an error if sold date is before bought date.

--- src/NewProject3/soldOnDialog.java
+++ src/NewProject3/soldOnDialog.java
@@ -91,8 +91,11 @@ public class soldOnDialog extends JDialog implements ActionListener {
                 df.setLenient(false);
                 d = df.parse(txtDate.getText());
                 temp.setTime(d);
-            } catch (ParseException e1) {
+                if(temp.compareTo(this.auto.getBoughtOn()) < 0)
+                    throw new Exception();
+            } catch (Exception e1) {
                 JOptionPane.showMessageDialog(null, "Invalid date. Setting date to default day.", "ERROR", JOptionPane.ERROR_MESSAGE);
+                closeStatus = CANCEL;
             }
             auto.setNameOfBuyer(txtName.getText());
             auto.setSoldOn(temp);
